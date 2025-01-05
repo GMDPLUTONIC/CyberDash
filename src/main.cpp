543c955a7379406f40cb765f04eb388e18b2965b
AcerCo <Elijahsaurusrex0702@gmail.com>
2025-01-05 04:31:40 -0600

    pluh

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,74 @@
 
 using namespace geode::prelude;
 
-#include <Geode/modify/LevelEditorLayer.hpp>
-class $modify(MyLevelEditorLayer, LevelEditorLayer) {
+#include <Geode/modify/MenuLayer.hpp>
+
+class $modify(MenuLayer){
+    bool init() {
+        if (!MenuLayer::init()) return false;
+
+        if (!Mod::get()->setSavedValue("seen-intro-popup", true)) {
+            FLAlertLayer* popup = FLAlertLayer::create(
+                "SpaceMenu",
+                "Welcome to SpaceMenu!\n<cb>Use the Page Down key or press the button.</c>",
+                "OK"
+            );
+            popup->m_scene = this;
+            popup->show();
+        }
+	}
+}
+
+class $modify(MyMenuLayer, MenuLayer) {
+
 	bool init() {
-		if (!LevelEditorLayer::init()) {
+		if (!MenuLayer::init()) {
 			return false;
 		}
-
 		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
 
 		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_chatBtn_001.png"),
+			CCSprite::createWithSpriteFrameName("main_btn.png"),
 			this,
-			menu_selector(MyLevelEditorLayer::onRampppant)
+			/**
+			 * Here we use the name we set earlier for our modify class.
+			*/
+			menu_selector(MyMenuLayer::onMyButton)
 		);
 
+		/**
+		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
+		 * Node IDs are a Geode feature, see this page for more info about it:
+		 * https://docs.geode-sdk.org/tutorials/nodetree
+		*/
 		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(Rampppant);
+		menu->addChild(myButton);
 
-		Rampppant->setID("Rampppant"_spr);
+		/**
+		 * The `_spr` string literal operator just prefixes the string with
+		 * your mod id followed by a slash. This is good practice for setting your own node ids.
+		*/
+		myButton->setID("my-button"_spr);
 
+		/**
+		 * We update the layout of the menu to ensure that our button is properly placed.
+		 * This is yet another Geode feature, see this page for more info about it:
+		 * https://docs.geode-sdk.org/tutorials/layouts
+		*/
 		menu->updateLayout();
 
+		/**
+		 * We return `true` to indicate that the class was properly initialized.
+		 */
 		return true;
 	}
 
-	void onRampppant(CCObject*) {
-		FLAlertLayer::create("Rampppant", "<cr>Welcome To Rampppant</c>. You Can Access The Lists In The Corner Of The Screen.", "OK")->show();
+	/**
+	 * This is the callback function for the button we created earlier.
+	 * The signature for button callbacks must always be the same,
+	 * return type `void` and taking a `CCObject*`.
+	*/
+	void onMyButton(CCObject*) {
+		FLAlertLayer::create("Rampppant", "<cr>Welcome To Rampppant!</c>You Can Open Rampppant", "OK")->show();
 	}
 };
